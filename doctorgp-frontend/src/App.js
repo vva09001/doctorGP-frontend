7cfb42f1bf09dce1410d2d1e4d7b3818d28b9d80
vva0901 <haidangvu07@gmail.com>
2019-02-15 18:11:01 +0700

    assignment 1

--- doctorgp-frontend/src/App.js
+++ doctorgp-frontend/src/App.js
@@ -2,19 +2,41 @@ import React, { Component } from 'react';
 import Information from '../src/component/ss01/Information';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      preson: [
+        { name: "Admin", age: 20 },
+        { name: "ReactJS", age: 5 }
+      ],
+      number: 0,
+      text: '',
+    }
+  }
+  clickHandler(value) {
+    this.setState({
+      number: (this.state.number + value >= 0) ? this.state.number + value : this.state.number
+    });
+  }
+  onChangeName = (value) => {
+    this.setState({
+      text : value,
+    })
+  }
   render() {
     return (
       <div className="App">
-          <Information
-            name = "Vũ Việt Anh"
-            age = "22"
-          
-          />
-           <Information
-            name = "Vũ Hải Đăng"
-            age = "10"
-          />
-          
+        <Information name="Vũ Việt Anh" age="22" text={this.state.text} changeName={this.onChangeName} />
+        <Information name="Vũ Hải Đăng" age="10" text={this.state.text} changeName={this.onChangeName} />
+        <Information name={this.state.preson[0].name} age={this.state.preson[0].age} text={this.state.text} changeName={this.onChangeName} />
+        <Information name={this.state.preson[1].name} age={this.state.preson[1].age} text={this.state.text} changeName={this.onChangeName}>Reacjs props children</Information>
+        <div>
+          <div className="table">
+            Number:  {this.state.number}
+          </div>
+          <button onClick={() => this.clickHandler(1)}>Add number</button>
+          <button onClick={() => this.clickHandler(-1)}>Remove number</button>
+        </div>
       </div>
     );
   }
--- doctorgp-frontend/src/component/ss01/Information.js
+++ doctorgp-frontend/src/component/ss01/Information.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import './Information.css';
 
 class Information extends Component {
-
+    changeName = (event) => {
+        if (event.key === "Enter") {
+            this.props.changeName(event.target.value);
+        }
+    }
     render() {
-        return(
+        const style = {
+            height : '10px',
+            color : 'red'
+        }
+        return (
             <div className="person">
                 <h1>{this.props.name}</h1>
                 <h2>Age: {this.props.age}</h2>
+                <p>{this.props.children}</p>
+                <h2>{this.props.text}</h2>
+                <input style={style} onKeyPress={this.changeName} type="text"  />
             </div>
         )
     }
